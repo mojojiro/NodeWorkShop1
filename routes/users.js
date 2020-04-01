0b48836d6a6d23f10134e80edef5510ed825fdea
Worawit Sutsue <worawitjo@gmail.com>
2020-04-01 22:12:32 +0700

    login and register form

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', function(req, res, next) {
+  console.log(req.body.name);
+  console.log(req.body.email);
+  console.log(req.body.password);
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  console.log(req.body.username);
+  console.log(req.body.password);
+});
+
 module.exports = router;
